3b38890de46d4eff2d7018c9d1189a658a311a78
Touseef Elahi <touseefelahi@ymail.com>
2019-12-18 17:06:31 +0300

    Add project files.

---
+++ Stira.EasySocketControl/Views/Control.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Stira.Socket.Interfaces;
+using System.Windows.Controls;
+
+namespace Stira.EasySocket.Views
+{
+    /// <summary>
+    /// Interaction logic for Control
+    /// </summary>
+    public partial class Control : UserControl
+    {
+        public Control()
+        {
+            InitializeComponent();
+            DataContext = new Socket.ViewModels.ControlViewModel();
+        }
+    }
+}
