b534b72165cc9bd9d590ff12c7ccf5a7e6cdad29
Touseef Elahi <touseefelahi@ymail.com>
2019-12-18 17:19:05 +0300

    Sample project working

--- Stira.EasySocket.Sample/ViewModels/MainWindowViewModel.cs
+++ Stira.EasySocket.Sample/ViewModels/MainWindowViewModel.cs
@@ -1,19 +1,22 @@
 ﻿using Prism.Mvvm;
+using Stira.Socket.Interfaces;
+using Stira.Socket.ViewModels;
 
 namespace Stira.EasySocket.Sample.ViewModels
 {
     public class MainWindowViewModel : BindableBase
     {
-        private string _title = "Prism Application";
-        public string Title
-        {
-            get { return _title; }
-            set { SetProperty(ref _title, value); }
-        }
+        private string _title = "Sample Stira Socket";
 
         public MainWindowViewModel()
         {
-
+            SocketControl = new ControlViewModel
+            {
+                IsHex = true,
+                DeveloperCommand = "010203"
+            };
         }
+
+        public IControlViewModel SocketControl { get; set; }
     }
 }
