407bc78f4159fb6ef7fbeedd50054cdd6397552a
Touseef Elahi <touseefelahi@ymail.com>
2020-07-28 13:08:24 +0300

    readme added

--- Stira.EasySocket.Sample/ViewModels/MainWindowViewModel.cs
+++ Stira.EasySocket.Sample/ViewModels/MainWindowViewModel.cs
@@ -6,8 +6,6 @@ namespace Stira.EasySocket.Sample.ViewModels
 {
     public class MainWindowViewModel : BindableBase
     {
-        private string _title = "Sample Stira Socket";
-
         public MainWindowViewModel()
         {
             SocketControl = new ControlViewModel
@@ -15,8 +13,19 @@ namespace Stira.EasySocket.Sample.ViewModels
                 IsHex = true,
                 DeveloperCommand = "010203"
             };
+            SocketControl.TcpClient.Connect("192.168.10.227", 4040, new byte[] { 0xa, 0xb });
+            SocketControl.TcpClient.DataEvent += DataIncoming;
         }
 
         public IControlViewModel SocketControl { get; set; }
+
+        /// <summary>
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void DataIncoming(object sender, IReplyPacket e)
+        {
+            var sent = await SocketControl.TcpClient.SendAsync(e.Reply.ToArray()).ConfigureAwait(false);
+        }
     }
 }
