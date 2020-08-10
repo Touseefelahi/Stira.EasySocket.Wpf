95af462bd90ce10adecead736a709315fd06835e
Touseef Elahi <touseefelahi@ymail.com>
2020-08-10 14:10:20 +0300

    Tcp Client Example

--- Stira.EasySocket.Sample/ViewModels/MainWindowViewModel.cs
+++ Stira.EasySocket.Sample/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using Prism.Mvvm;
 using Stira.Socket.Interfaces;
+using Stira.Socket.Models;
 using Stira.Socket.ViewModels;
 
 namespace Stira.EasySocket.Sample.ViewModels
@@ -13,16 +14,19 @@ namespace Stira.EasySocket.Sample.ViewModels
                 IsHex = true,
                 DeveloperCommand = "010203"
             };
+            ITcpClient tcpClient = new EasyTcpClient
+            {
+                Ip = "192.168.10.227",
+                Port = 4040
+            };
+            var endBytesIdentifier = new byte[] { 0x0a, 0x0b };
+            tcpClient.Connect(endBytesIdentifier);
             SocketControl.TcpClient.Connect("192.168.10.227", 4040, new byte[] { 0xa, 0xb });
             SocketControl.TcpClient.DataEvent += DataIncoming;
         }
 
         public IControlViewModel SocketControl { get; set; }
 
-        /// <summary>
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
         private async void DataIncoming(object sender, IReplyPacket e)
         {
             var sent = await SocketControl.TcpClient.SendAsync(e.Reply.ToArray()).ConfigureAwait(false);
